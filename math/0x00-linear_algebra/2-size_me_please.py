c73d5ec888a29227c75269a03a6533345fb03b70
Haroldo Velez <haroldo.velez@rappi.com>
2020-12-24 12:22:09 -0500

    [fix] pydocstyle

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Mod for compute matrix shape."""
 
 def matrix_shape(matrix):
+    """Func to compute matrix shape."""
     dims = []
     while True:
         dims.append(len(matrix))
